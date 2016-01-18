72a13ffa32325d8b890d3ac123d0b651afb7c719
sagerman4 <sager.davidson@gmail.com>
2016-01-18 11:30:22 -0600

    Removed some admin junk from the app.

--- server/coinLaundryApp.js
+++ server/coinLaundryApp.js
@@ -2,7 +2,6 @@
 
 var routes = require('./routes'),
     user = require('./routes/user'),
-    admin = require('./routes/admin'),
     api = require('./routes/api'),
     _ = require('underscore'),
     oa = require('./utils/oauth');
@@ -24,10 +23,6 @@ FantasyApp.prototype.setupRoutes = function() {
     this.app.get('/logout', routes.logout);
     this.app.get('/authorize', routes.authorize);
     
-    // Admin Routes
-    this.app.get('/api/admin/authenticate/:id', admin.isAdmin);
-    this.app.get('/api/admin/create/:id', admin.createAdmin);
-
     // API Routes
     this.app.get('/api/users', user.list);
     _.each(api, function(route, name) {
