8326ba67928290cb04a6384266cad285f2078e43
sagerman4 <sager.davidson@gmail.com>
2016-01-23 22:57:00 -0600

    Got calls out to get all weeks of stats for players.  This Yahoo! API is so inefficient...pretty painful.

--- webapp/app/reports/bestdraft.js
+++ webapp/app/reports/bestdraft.js
@@ -17,37 +17,70 @@ angular.module('app.bestdraft').controller('BestDraftController', ['$scope', '$s
         $scope.teams = data;
         LeagueService.getDraftResults($scope.league.league_key).then(function(data){
             $scope.draftResults = data.fantasy_content.league[1].draft_results;
-            $scope.playerKeys = [];
+            $scope.keys = [];
             _.each($scope.draftResults, function(result) {
                 // var player = PlayersService.getPlayer(result.draft_result.player_key);
                 if(result.draft_result){
-                    $scope.playerKeys.push(result.draft_result.player_key);
+                    $scope.keys.push(result.draft_result.player_key);
                 }   
             });
 
-            $scope.players = [];
+            $scope.week = 0;
+            var weeks = [];
+            weeks.push({players: [], keys: $scope.keys.slice(0)});
+            weeks.push({players: [], keys: $scope.keys.slice(0)});
+            weeks.push({players: [], keys: $scope.keys.slice(0)});
+            weeks.push({players: [], keys: $scope.keys.slice(0)});
+            weeks.push({players: [], keys: $scope.keys.slice(0)});
+            weeks.push({players: [], keys: $scope.keys.slice(0)});
+            weeks.push({players: [], keys: $scope.keys.slice(0)});
+            weeks.push({players: [], keys: $scope.keys.slice(0)});
+            weeks.push({players: [], keys: $scope.keys.slice(0)});
+            weeks.push({players: [], keys: $scope.keys.slice(0)});
+            weeks.push({players: [], keys: $scope.keys.slice(0)});
+            weeks.push({players: [], keys: $scope.keys.slice(0)});
+            weeks.push({players: [], keys: $scope.keys.slice(0)});
+            weeks.push({players: [], keys: $scope.keys.slice(0)});
+            weeks.push({players: [], keys: $scope.keys.slice(0)});
+            weeks.push({players: [], keys: $scope.keys.slice(0)});
 
-            // var callback = function(data){
-            //   $scope.
-            // };
+            var callback = function(data){
+              console.log(data);
+              _.each(data, function(player){
+                var element = {key: player.player_key, points: player.stats.total};
+                weeks[$scope.week].players.push(element);
+              });
+              weeks[$scope.week].keys.splice(0,25);
 
-            PlayersService.getWeeklyStats($scope.league.league_key, $scope.playerKeys, '1').then(function(data){
-              console.log('something', data);
-            });
+              console.log('$scope.week', $scope.week);
+
+              if(weeks[$scope.week].players.length===$scope.keys.length){
+                $scope.week++;
+              }
+              if($scope.week!==16){
+                PlayersService.getWeeklyStats($scope.league.league_key, weeks[$scope.week].keys, $scope.week+1).then(callback);
+              }
+              if($scope.week===16){
+                console.log('177777777777777777', weeks);
+              }
+              return;
+            };
+
+            PlayersService.getWeeklyStats($scope.league.league_key, weeks[$scope.week].keys, $scope.week+1).then(callback);
 
             // $scope.playerResults = [];
 
             // var callback = function(data){
             //     $scope.playerResults = $scope.playerResults.concat(data);
-            //     if(data.length===$scope.playerKeys.length){
+            //     if(data.length===$scope.keys.length){
             //         $scope.organizeIntoTeams();
             //         return;
             //     }
-            //     $scope.playerKeys.splice(0,25);
-            //     PlayersService.getPlayers(league.league_key, $scope.playerKeys).then(callback);
+            //     $scope.keys.splice(0,25);
+            //     PlayersService.getPlayers(league.league_key, $scope.keys).then(callback);
             // };
 
-            // PlayersService.getPlayers(league.league_key, $scope.playerKeys).then(callback);
+            // PlayersService.getPlayers(league.league_key, $scope.keys).then(callback);
 
 
         });
