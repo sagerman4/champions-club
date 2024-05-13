07515f0b4a6d186b6bb7290584fc159786bff44b
sager <sager@sagers-mbp.myfiosgateway.com>
2024-05-13 19:49:17 -0400

    Added port management

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { UseFilters } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
