4b6934ef7d3a57c6bf31f754c5f5f39b8089565c
sager <sager@sagers-mbp.myfiosgateway.com>
2024-05-14 15:57:43 -0400

    Another one

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Data source to check if the Elastic Beanstalk application already exists
-data "aws_elastic_beanstalk_application" "existing" {
-  name = "champions-club-application"
-}
-
-# Conditional creation of the Elastic Beanstalk application
+# Elastic Beanstalk application
 resource "aws_elastic_beanstalk_application" "myapp" {
-  count = can(data.aws_elastic_beanstalk_application.existing.id) ? 0 : 1
-
   name        = "champions-club-application"
   description = "Champions Club Nest.js Application"
 
@@ -19,13 +12,12 @@ resource "aws_elastic_beanstalk_application" "myapp" {
   }
 }
 
-# Elastic Beanstalk environment, using the application name from either the new or existing application
+# Elastic Beanstalk environment using the application
 resource "aws_elastic_beanstalk_environment" "myenv" {
   name                = "ChampionsClub-env"
-  application         = coalesce(aws_elastic_beanstalk_application.myapp[0].name, data.aws_elastic_beanstalk_application.existing.name)
+  application         = aws_elastic_beanstalk_application.myapp.name
   solution_stack_name = "64bit Amazon Linux 2023 v6.1.4 running Node.js 20"
-  wait_for_ready_timeout = "30m"  // Increase from '20m' to '30m'
-  depends_on = [aws_elastic_beanstalk_application.myapp]
+  wait_for_ready_timeout = "30m"
 
   setting {
     namespace = "aws:elasticbeanstalk:environment:process:default"
