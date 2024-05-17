d0fa45d41d095d1436ddc071156395f3faaeb283
sager <sager@sagers-mbp.myfiosgateway.com>
2024-05-16 22:45:16 -0400

    Updated port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ import { ConfigModule } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
